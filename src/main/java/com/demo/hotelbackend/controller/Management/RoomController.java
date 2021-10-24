46e93134c65998a3f57cfaf10b9924d7bb117fd4
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-10-23 20:42:20 -0500

    FEAT Update controller of room. (correction)

--- src/main/java/com/demo/hotelbackend/controller/Management/RoomController.java
+++ src/main/java/com/demo/hotelbackend/controller/Management/RoomController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -90,7 +91,7 @@ public class RoomController {
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping("/changeState/{idroomm}")
+    @PutMapping("/changeState/{idroomm}")
     public Mono<ResponseEntity<Map<String, Object>>> changeState(@PathVariable("idroomm") String idroomm) {
         return service
             .changeState(idroomm)
